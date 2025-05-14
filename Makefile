355868c512513a15ed8c8abdac7bc1e45de60ec4
Bardolomeo <sapiobard@gmail.com>
2025-05-14 21:33:30 +0200

    Mariadb working, started with wordpress

--- Makefile
+++ Makefile
@@ -1,16 +1,36 @@
 
 
-cont?=mariadb
+container?=wordpress
+volume?=wordpress
+image?=wordpress
+
+
+fupd:
+	cd srcs ; sudo docker compose up --build -d ; cd ..
 
 fup:
-	cd srcs ; sudo docker compose up --build -d ; cd ..
+	cd srcs ; sudo docker compose up --build ; cd ..
+
+fre:	fdown fup
+
+down:
+	cd srcs ; sudo docker compose down ; cd ..
+
+downi:
+	cd srcs ; sudo docker compose down ; sudo docker rmi inception-$(image); cd ..
+
 
 fdown:
 	cd srcs ; sudo docker compose down -v --rmi all ; cd ..
 
+rm-vol:
+	sudo rm -rf /home/gsapio/data/$(volume)/*
+
 rm-vol-all:
-	sudo rm /home/gsapio/data/*/*
+	sudo rm -rf /home/gsapio/data/*/*
 
 conshell:
-	sudo docker exec -ti $(cont) bash 
+	sudo docker exec -ti $(container) bash 
 
+prune:
+	sudo docker system prune --volumes
--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-echo well
-service mysql start
-# echo "CREATE DATABASE IF NOT EXISTS $db1_name ;" > db1.sql
-# echo "CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;" >> db1.sql
-# echo "GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;" >> db1.sql
-# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY $sql_pass ;" >> db1.sql
-# echo "FLUSH PRIVILEGES;" >> db1.sql
 
-# mysql -u root -p $sql_pass 
-# < db1.sql
 
-kill $(cat /var/run/mysqld/mysqld.pid)
+service mysql start 
 
-mysqld
+
+echo "CREATE DATABASE IF NOT EXISTS $db1_name ;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;" >> db1.sql
+echo "FLUSH PRIVILEGES;" >> db1.sql
+#
+cat /etc/mysql/mariadb.conf.d/50-server.cnf | grep bind-address
+#
+mysql < db1.sql
+#
+#
+#kill $(cat /var/run/mysqld/mysqld.pid)
+
+#mysqld
